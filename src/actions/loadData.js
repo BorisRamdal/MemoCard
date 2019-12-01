57616471da01447208be5c0e34102a71cbd81a78
BorisRamdal <boris.ramdal@gmail.com>
2019-12-02 01:41:12 +0200

    done

--- src/actions/loadData.js
+++ src/actions/loadData.js
@@ -1,18 +1,12 @@
 export default function (activeRange){
     return dispatch => {
-        if(localStorage.length > 0) {
-            let data = localStorage.getItem('data');
-            let storeData = JSON.parse(data);
-            dispatch({type: 'LOAD_STORE', payload: storeData});
-        } else {
-            fetch(`./data/${activeRange}.json`)
-                .then(response => response.json())
-                .then(data => dispatch({type: 'LOAD_RANGE', payload: data}))
-                .catch(err => console.log('Fetch error: ', err));
-            fetch(`./data/total.json`)
-                .then(response => response.json())
-                .then(data => dispatch({type: 'LOAD_RANGES', payload: data}))
-                .catch(err => console.log('Fetch error: ', err))
-        }
+        fetch(`./data/${activeRange}.json`)
+            .then(response => response.json())
+            .then(data => dispatch({type: 'LOAD_RANGE', payload: data}))
+            .catch(err => console.log('Fetch error: ', err));
+        fetch(`./data/total.json`)
+            .then(response => response.json())
+            .then(data => dispatch({type: 'LOAD_RANGES', payload: data}))
+            .catch(err => console.log('Fetch error: ', err))
     }
 }
