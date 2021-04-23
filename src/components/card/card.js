4057c9074d0a0fa52651554f049b0c42ad38e176
BorisRamdal <boris.ramdal@gmail.com>
2021-04-23 21:02:30 +0300

    refactoring

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,35 +1,26 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import {withRouter} from 'react-router-dom';
 import './card.scss';
 
-class Card extends Component {
-    state = {
-        open: false
-    };
+function Card (props) {
+    const [open, setIsOpen] = useState(false)
 
-    showTranslate = () => {
-        this.setState({
-            open: !this.state.open
-        });
-    };
-    render() {
-        const {pathname} = this.props.location;
-        const {word, translation, addtosave, removefromsave } = this.props;
-        return(
-            <div className="card">
-                <div className="title">{word}</div>
-                <div className="trans-box">
-                    <span onClick={this.showTranslate}>
-                        {this.state.open ? translation : '?'}
-                    </span>
-                </div>
-                {pathname === '/saved' ?
-                    <button className="btn" onClick={()=> removefromsave(word)}>Remove</button> :
-                    <button className="btn" onClick={()=> addtosave(word, translation)}>Add to learned</button>
-                }
+    const {pathname} = props.location;
+    const {word, translation, addtosave, removefromsave } = props;
+    return(
+        <div className="card">
+            <div className="title">{word}</div>
+            <div className="trans-box">
+                <span onClick={()=> setIsOpen(!open)}>
+                    {open ? translation : '?'}
+                </span>
             </div>
-        )
-    }
+            {pathname === '/saved' ?
+                <button className="btn" onClick={()=> removefromsave(word)}>Remove</button> :
+                <button className="btn" onClick={()=> addtosave(word, translation)}>Add to learned</button>
+            }
+        </div>
+    )
 }
 
 export default withRouter(Card);
